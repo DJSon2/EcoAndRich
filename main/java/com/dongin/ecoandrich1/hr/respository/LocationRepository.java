c94b5fb883937f6f352dc7507a0482506017df8f
DJSon2 <124123956+DJSon2@users.noreply.github.com>
2023-04-26 02:00:51 +0900

    Delete main directory

--- main/java/com/dongin/ecoandrich1/hr/respository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dongin.ecoandrich1.hr.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dongin.ecoandrich1.hr.entity.Location;
-
-public interface LocationRepository extends JpaRepository <Location, Long>{
-
-}
